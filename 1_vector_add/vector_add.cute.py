09bf210d0985b2fbdb364f4876b2d8eea3da7d95
licao <licao@andrew.cmu.edu>
2025-12-13 21:27:59 -0500

    Delete vector_add.cute.py

--- 1_vector_add/vector_add.cute.py
+++
@@ -1,9 +0,0 @@
-import cutlass
-import cutlass.cute as cute
-
-# A, B, C are tensors on the GPU
-@cute.jit
-def solve(A: cute.Tensor, B: cute.Tensor, C: cute.Tensor, N: cute.Uint32):
-    idx = cute.threadIdx.x + cute.blockIdx.x * cute.blockDim.x
-    if idx < N:
-        C[idx] = A[idx] + B[idx]
